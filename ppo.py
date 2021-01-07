2b8fac21131e68dcf0288459a4895939492e3f0d
Justin Terry <justinkterry@gmail.com>
2021-01-07 14:12:14 -0500

    make own agent class to fix import

--- ppo.py
+++ ppo.py
@@ -2,7 +2,7 @@ import torch
 from torch.nn.functional import mse_loss
 from all.logging import DummyWriter
 from all.memory import GeneralizedAdvantageBuffer
-from all._agent import Agent
+from _agent import Agent
 
 
 class PPO(Agent):
