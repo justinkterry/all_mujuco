4e9bf5df52e42d0c82e3c61f647e50de678c9a52
Justin Terry <justinkterry@gmail.com>
2021-01-09 22:02:48 -0500

    move hyperparameters to dict

--- run.py
+++ run.py
@@ -26,29 +26,29 @@ def modified_fc_actor_critic(env, hidden1=64, hidden2=64):
     return features, v, policy
 
 
-ppo = ppo(
-    discount_factor=0.99,
-    last_frame=1e6,
+hyperparameters = {
+    'discount_factor':0.99,
+    'last_frame':1e6,
     # Adam optimizer settings
-    lr=3e-4,  # Adam learning rate
-    eps=1e-5,  # Adam stability
+    'lr':3e-4,  # Adam learning rate
+    'eps':1e-5,  # Adam stability
     # Loss scaling
-    entropy_loss_scaling=0.0,
-    value_loss_scaling=0.5,
+    'entropy_loss_scaling':0.0,
+    'value_loss_scaling':0.5,
     # Training settings
-    clip_grad=0.5,
-    clip_initial=0.2,
-    clip_final=0.2,
-    epochs=15,
-    minibatches=1,  # not entirely sure if this is correct
+    'clip_grad':.5,
+    'clip_initial':0.2,
+    'clip_final':0.2,
+    'epochs':15,
+    'minibatches':1,  # not entirely sure if this is correct
     # Batch settings
-    n_envs=16,  # probably 32?
-    n_steps=2048,
+    'n_envs':16,  # probably 32?
+    'n_steps':2048,
     # GAE settings
-    lam=0.95,
+    'lam':0.95,
     # Model construction
-    ac_model_constructor=modified_fc_actor_critic)
+    'ac_model_constructor':modified_fc_actor_critic}
 
 
-run_experiment([ppo()], [GymEnvironment('InvertedPendulum-v2', device='cuda')], frames=1e6)
+run_experiment([ppo(hyperparameters)], [GymEnvironment('InvertedPendulum-v2', device='cuda')], frames=1e6)
 plot_returns_100('runs', timesteps=1e6)
